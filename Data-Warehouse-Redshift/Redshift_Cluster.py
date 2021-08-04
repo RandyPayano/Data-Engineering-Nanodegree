8903555400e02d8a9f1ce765a485eff8f4baa1ae
randypayano@gmail.com <randypayano@gmail.com>
2021-08-04 13:52:26 -0400

    vpc security group creator func ready

--- Data-Warehouse-Redshift/Redshift_Cluster.py
+++ Data-Warehouse-Redshift/Redshift_Cluster.py
@@ -5,6 +5,15 @@ import psycopg2
 import configparser
 
 def create_redshift_cluster(cfg_file_path, redshift_client):
+    """Creates AWS redshift cluster
+
+    Args:
+        cfg_file_path (string): Path to configuration file
+        redshift_client ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
 
     config = configparser.ConfigParser()
     config.read_file(open(cfg_file_path))
@@ -22,28 +31,37 @@ def create_redshift_cluster(cfg_file_path, redshift_client):
     DWH_PORT               = config.get("DWH","DWH_PORT")
     DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
 
-
+    # Check if cluster already exists
     try:
-        print("-"*15, "Creating Cluster")
-        response = redshift_client.create_cluster(        
-            # hardware
-            ClusterType=DWH_CLUSTER_TYPE,
-            NodeType=DWH_NODE_TYPE,
-            NumberOfNodes=int(DWH_NUM_NODES),   
-            
-            # identifiers & credentials
-                DBName=DWH_DB,
-                ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
-                MasterUsername=DWH_DB_USER,
-                MasterUserPassword=DWH_DB_PASSWORD,
-            
-            # parameter for role (to allow s3 access)
-            IamRoles=[roleArn]
-        
-        )
-        print("Redshift Cluster created")
-    except Exception as e:
-        response = e 
-        print(e)
+        response = redshift_client.describe_clusters(ClusterIdentifier=config.get('CLUSTER', 'CLUSTERIDENTIFIER'))
+        print('Cluster already exists: ' + response['Clusters'][0]['ClusterIdentifier'])
+        return None
+    except:
+        response = None
 
-    return response 
+    # If not create Cluster
+    if response is None:
+        try:
+            print("-"*15, "Creating Cluster")
+            response = redshift_client.create_cluster(        
+                # hardware
+                ClusterType=DWH_CLUSTER_TYPE,
+                NodeType=DWH_NODE_TYPE,
+                NumberOfNodes=int(DWH_NUM_NODES),   
+                
+                # identifiers & credentials
+                    DBName=DWH_DB,
+                    ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+                    MasterUsername=DWH_DB_USER,
+                    MasterUserPassword=DWH_DB_PASSWORD,
+                
+                # parameter for role (to allow s3 access)
+                IamRoles=[roleArn]
+            
+            )
+            print("Redshift Cluster created")
+            return response['Cluster']
+
+        except Exception as e:
+            print(f"Error {e}")
+            return None
